f24068f6c5a07042247a24cd9db8a026adfe2f5f
RoterHund <alan@cointegrate.com>
2024-04-06 18:00:10 +0200

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,17 +1,20 @@
 from django.contrib import admin
 from .models import CarMake, CarModel
 
+
 class CarModelInline(admin.TabularInline):
     model = CarModel
     extra = 1
+
 
 class CarMakeAdmin(admin.ModelAdmin):
     fields = ['name', 'description', 'country']
     inlines = [CarModelInline]
 
+
 class CarModelAdmin(admin.ModelAdmin):
     fields = ['name', 'type', 'year']
 
+
 admin.site.register(CarMake, CarMakeAdmin)
 admin.site.register(CarModel, CarModelAdmin)
-
